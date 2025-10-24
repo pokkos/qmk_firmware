b1be57553ffe8e15880d7eb9f04007cf8ad08372
pokkos <pokkos@tuta.io>
2025-10-25 00:34:25 +0200

    Switch Space/Lower and Enter/Raise keys

--- keyboards/sofle_pico/keymaps/pokkos/keymap.c
+++ keyboards/sofle_pico/keymaps/pokkos/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
         * |LShift|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |RShift|
         * `-----------------------------------------/       /     \      \-----------------------------------------'
-        *            | LGUI | LAlt | LCTR |Space | / LOWER /       \RAISE \  |Enter | RCTR | RAlt | RGUI |
+        *            | LGUI | LAlt | LCTR |LOWER | / Space /       \Enter \  |RAISE | RCTR | RAlt | RGUI |
         *            |      |      |      |      |/       /         \      \ |      |      |      |      |
         *            `----------------------------------'           '------''---------------------------'
         */
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                          KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSPC,
         KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                          KC_M,    KC_N,    KC_E,    KC_I,   KC_O,    KC_QUOT,
         KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,    KC_MUTE,     XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                          KC_LGUI, KC_LALT, KC_LCTL, KC_SPC, TL_LOWR,      TL_UPPR, KC_ENT , KC_RCTL, KC_RALT, KC_RGUI
+                          KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_SPC,      KC_ENT , TL_UPPR,  KC_RCTL, KC_RALT, KC_RGUI
         ),
 
         /*
