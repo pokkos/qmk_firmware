5ca63dae07eda83d3c463ba78d17b2ad0f0a22f1
pokkos <pokkos@tuta.io>
2025-10-27 00:24:54 +0100

    Mod-Tap Ctrl/Esc and Shift/Tab

--- keyboards/sofle_pico/keymaps/pokkos/keymap.c
+++ keyboards/sofle_pico/keymaps/pokkos/keymap.c
@@ -49,16 +49,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
         * |LShift|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |RShift|
         * `-----------------------------------------/       /     \      \-----------------------------------------'
-        *            | LGUI | LAlt | LCTR |LOWER | / Space /       \Enter \  |RAISE | RCTR | RAlt | RGUI |
-        *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+        *            | LGUI | LAlt | LCTL-|LOWER | / Space /       \Enter \  |RAISE |Shift-| RAlt | RGUI |
+        *            |      |      | ESC  |      |/       /         \      \ |      |TAB   |      |      |
         *            `----------------------------------'           '------''---------------------------'
         */
         [_COLEMAK_DH] = LAYOUT(
-        KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_GRV,
-        KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                          KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSPC,
-        KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                          KC_M,    KC_N,    KC_E,    KC_I,   KC_O,    KC_QUOT,
-        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,    KC_MUTE,     XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                          MO(_RAISE), KC_LALT, KC_LCTL, MO(_LOWER), KC_SPC,      KC_ENT , MO(_RAISE),  KC_RCTL, KC_RALT, KC_RGUI
+        KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                 KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_GRV,
+        KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                                 KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSPC,
+        KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                                 KC_M,    KC_N,    KC_E,    KC_I,   KC_O,    KC_QUOT,
+        KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,    KC_MUTE,            XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+              MO(_RAISE), KC_LALT, CTL_T(KC_ESC), MO(_LOWER), KC_SPC,      KC_ENT , MO(_RAISE),  SFT_T(KC_TAB), KC_RALT, KC_RGUI
         ),
 
         /*
