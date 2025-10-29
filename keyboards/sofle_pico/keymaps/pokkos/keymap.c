ac82b4d7b6575cd970d4072f92e0edf5ed8b9c8f
pokkos <pokkos@tuta.io>
2025-10-29 01:26:26 +0100

    Set Shift as a home row mod

--- keyboards/sofle_pico/keymaps/pokkos/keymap.c
+++ keyboards/sofle_pico/keymaps/pokkos/keymap.c
@@ -53,6 +53,8 @@ tap_dance_action_t tap_dance_actions[] = {
 #define TG_ADJST TG(_ADJUST)
 #define L_GUISPC LT(_RAISE,KC_SPC)
 #define L_GUIENT LT(_RAISE,KC_ENT)
+#define MT_T_SFT LSFT_T(KC_T)
+#define MT_N_SFT LSFT_T(KC_N)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
@@ -78,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                            KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-    KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                            KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    KC_ESC,  KC_A,    KC_R,    KC_S,    MT_T_SFT,KC_G,                            KC_M,    MT_N_SFT,KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    TG_ADJST,      TG_ADJST,KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                       TH_HIGH, KC_LALT, TH_CTESC,TH_LOW,  L_GUISPC,      L_GUIENT,TH_HIGH, KC_RSFT, TH_TDALT,KC_RGUI
     ),
