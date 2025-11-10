5af6ace4ca077c5cb7ac79c6479329ca535f3624
pokkos <pokkos@tuta.io>
2025-11-10 09:10:13 +0100

    Remove the mod tap from the Esc/Bspc keys

--- keyboards/sofle_pico/keymaps/pokkos/keymap.c
+++ keyboards/sofle_pico/keymaps/pokkos/keymap.c
@@ -56,9 +56,7 @@ tap_dance_action_t tap_dance_actions[] = {
 #define MO_HIGH MO(_RAISE)
 #define TD_ALT TD(TD_LALT_RALT)
 #define SPC_GUI LGUI_T(KC_SPC)
-#define ESC_CTL CTL_T(KC_ESC)
 #define ENT_GUI LGUI_T(KC_ENT)
-#define BSC_SFT RSFT_T(KC_BSPC)
 
 // home row mods for ColemakDH
 #define A_GUI LGUI_T(KC_A)
@@ -94,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |CW_TOG|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |      |
     * `-----------------------------------------/       /     \      \-----------------------------------------'
     *            | LGUI | LAlt | Esc  |Space | / LOWER /       \RAISE \  |Enter | Bspc | LAlt-| RGUI |
-    *            |      |      | LSFT |GUI   |/       /         \       \|GUI   | RSFT | RAlt |      |
+    *            |      |      |      |GUI   |/       /         \       \|GUI   |      | RAlt |      |
     *            `----------------------------------'           '------''---------------------------'
     */
     [_COLEMAK_DH] = LAYOUT(
@@ -102,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                            KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     KC_TAB,  A_GUI,   R_ALT,   S_CTL,   T_SFT,   KC_G,                            KC_M,    N_SFT,   E_CTL,   I_ALT,   O_GUI,   KC_QUOT,
     CW_TOGG, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_MUTE,       KC_ADJST,KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-                      KC_LGUI, KC_LALT, ESC_CTL, SPC_GUI, MO_LOW,        MO_HIGH, ENT_GUI, BSC_SFT, TD_ALT,  KC_RGUI
+                      KC_LGUI, KC_LALT, KC_ESC,  SPC_GUI, MO_LOW,        MO_HIGH, ENT_GUI, KC_BSPC, TD_ALT,  KC_RGUI
     ),
 
     /*
@@ -117,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |CW_TOG|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
     * `-----------------------------------------/       /     \      \-----------------------------------------'
     *            | LGUI | LAlt | Esc  |Space | / LOWER /       \RAISE \  |Enter | Bspc | LAlt-| RGUI |
-    *            |      |      | LSFT |GUI   |/       /         \      \ |GUI   | RSFT | RAlt |      |
+    *            |      |      |      |GUI   |/       /         \      \ |GUI   |      | RAlt |      |
     *            `----------------------------------'           '------''---------------------------'
     */
     [_QWERTY] = LAYOUT(
@@ -125,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  A_GUI,   S_ALT,   D_CTL,   F_SFT,   KC_G,                            KC_H,    J_SFT,   K_CTL,   L_ALT,   SCLN_GUI,KC_QUOT,
     CW_TOGG, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,       KC_ADJST,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-                      KC_LGUI, KC_LALT, ESC_CTL, SPC_GUI, MO_LOW,        MO_HIGH, ENT_GUI, BSC_SFT, TD_ALT,  KC_RGUI
+                      KC_LGUI, KC_LALT, KC_ESC,  SPC_GUI, MO_LOW,        MO_HIGH, ENT_GUI, KC_BSPC, TD_ALT,  KC_RGUI
     ),
 
     /* LOWER
