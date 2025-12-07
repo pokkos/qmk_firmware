e9470923fbd31f555b2974eefb6f68dc12ebfbdd
pokkos <pokkos@tuta.io>
2025-12-07 14:25:59 +0100

    Move the GAMING layer to the correct position

--- keyboards/sofle_pico/keymaps/pokkos/keymap.c
+++ keyboards/sofle_pico/keymaps/pokkos/keymap.c
@@ -28,10 +28,10 @@
 enum sofle_layers {
     _COLEMAK_DH,
     _QWERTY,
+    _GAMING,
     _LOWER,
     _RAISE,
     _ADJUST,
-    _GAMING,
 };
 
 enum custom_keycodes {
@@ -128,6 +128,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LGUI, KC_LALT, MO_LOW,  SPC_GUI, KC_ESC,        KC_BSPC, ENT_GUI, MO_HIGH, TD_ALT,  KC_RGUI
     ),
 
+    /*
+    * GAMING
+    * ,-----------------------------------------.                    ,-----------------------------------------.
+    * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Del  |
+    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+    * | Esc  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
+    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+    * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+    * |------+------+------+------+------+------|  MUTE |    | ADJST |------+------+------+------+------+------|
+    * |LShft |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
+    * `-----------------------------------------/       /     \      \-----------------------------------------'
+    *            |      | LAlt |LOWER |Space | /  Esc  /       \ Bsbc \  |Enter |RAISE | LAlt-|      |
+    *            |      |      |      |      |/       /         \      \ |GUI   |      | RAlt |      |
+    *            `----------------------------------'           '------''---------------------------'
+    */
+    [_GAMING] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
+    _______, KC_A,    KC_S,    KC_D,    KC_F,    _______,                         _______, KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
+    KC_LSFT, _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
+                      _______, _______, _______, KC_SPC,  _______,       _______, ENT_GUI, _______, TD_ALT,  _______
+    ),
+
     /* LOWER
     * ,-----------------------------------------.                    ,-----------------------------------------.
     * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
@@ -194,28 +217,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
     ),
 
-    /*
-    * GAMING
-    * ,-----------------------------------------.                    ,-----------------------------------------.
-    * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Del  |
-    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    * | Esc  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
-    * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
-    * |------+------+------+------+------+------|  MUTE |    | ADJST |------+------+------+------+------+------|
-    * |LShft |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
-    * `-----------------------------------------/       /     \      \-----------------------------------------'
-    *            |      | LAlt |LOWER |Space | /  Esc  /       \ Bsbc \  |Enter |RAISE | LAlt-|      |
-    *            |      |      |      |      |/       /         \      \ |GUI   |      | RAlt |      |
-    *            `----------------------------------'           '------''---------------------------'
-    */
-    [_GAMING] = LAYOUT(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,       KC_ADJST,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-                      XXXXXXX, KC_LALT, MO_LOW,  KC_SPC,  KC_ESC,        KC_BSPC, ENT_GUI, MO_HIGH, TD_ALT,  XXXXXXX
-    ),
     // clang-format on
 };
 
@@ -223,10 +224,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_COLEMAK_DH] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
     [_QWERTY] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
+    [_GAMING] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(_______, _______) },
     [_LOWER] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
     [_RAISE] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(_______, _______) },
     [_ADJUST] = { ENCODER_CCW_CW(DT_DOWN, DT_UP), ENCODER_CCW_CW(KC_BRIU, KC_BRID) },
-    [_GAMING] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(_______, _______) },
 };
 #endif
 
