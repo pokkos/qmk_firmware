9163d5a10d4280ad3827872bf4ac6e84b02a97d9
pokkos <pokkos@tuta.io>
2026-03-04 09:35:12 +0100

    Add Euro sign to the lower layer

--- keyboards/sofle_pico/keymaps/pokkos/keymap.c
+++ keyboards/sofle_pico/keymaps/pokkos/keymap.c
@@ -43,6 +43,7 @@ enum custom_keycodes {
     KC_UM_O,
     KC_UM_U,
     KC_SS,
+    KC_EURO,
 };
 
 enum tapdance_keys {
@@ -165,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
     * |      |   |  |   /  |   \  |   +  |   ?  |-------.    ,-------|   _  |   -  |   =  |   {  |   }  |      |
     * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-    * |      |   ß  |   ä  |   ö  |   ü  |   ~  |-------|    |-------|      |   <  |   >  |   [  |   ]  |      |
+    * |   €  |   ß  |   ä  |   ö  |   ü  |   ~  |-------|    |-------|      |   <  |   >  |   [  |   ]  |      |
     * `-----------------------------------------/       /     \      \-----------------------------------------'
     *            |      |      |      |      | /       /       \ LOCK \  |      |      |      |      |
     *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -175,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                           KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
     XXXXXXX, KC_PIPE, KC_SLSH, KC_BSLS, KC_PLUS, KC_QUES,                         KC_UNDS, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, XXXXXXX,
-    XXXXXXX, KC_SS,   KC_UM_A, KC_UM_O, KC_UM_U, KC_TILD, XXXXXXX,       XXXXXXX, XXXXXXX, KC_LT,   KC_GT,   KC_LBRC, KC_RBRC, XXXXXXX,
+    KC_EURO, KC_SS,   KC_UM_A, KC_UM_O, KC_UM_U, KC_TILD, XXXXXXX,       XXXXXXX, XXXXXXX, KC_LT,   KC_GT,   KC_LBRC, KC_RBRC, XXXXXXX,
                       _______, _______, _______, _______, _______,       QK_LLCK, _______, _______, _______, _______
     ),
 
@@ -281,6 +282,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_SS:
             if (record->event.pressed) {
                 SEND_STRING(SS_RALT("s"));
+            }
+            return false;
+        case KC_EURO:
+            if (record->event.pressed) {
+                SEND_STRING(SS_RALT("e"));
             }
             return false;
     }
